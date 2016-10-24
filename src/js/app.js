378d23c810894e725641df669656218441d758ba
Byron Buckley <byronbuckley@live.com>
2016-10-23 19:19:28 -0500

    app: demo, test and show

--- src/js/app.js
+++ src/js/app.js
@@ -3,11 +3,48 @@ import { bounds, X_BOUNDS, Y_BOUNDS } from './config';
 import Martian from './martian';
 import Robot from './martianRobot';
 import { default as Store } from './store';
-import { instruct, printMars, getMartians } from './controller';
+import { instruct, getMartians } from './controller';
 
 const vorpal = require('vorpal')();
 
 const mars = new Store();
+
+vorpal
+  .command('show')
+  .description('Show me who is on Mars.')
+  .option('-l', 'Show me lost Robots.')
+  .option('-r', 'Show me all Robots.')
+  .option('-m', 'Show me all Martians.')
+  .validate(function(args) {
+    if(mars.getAll().size === 0) {
+      return 'There is nothing on Mars, please run `instruct` then try again.';
+    }
+    else {
+      return true;
+    }
+  })
+  .action(function(args, callback) {
+    const self = this;
+
+    if(!args.options.l && !args.options.r && !args.options.m) {
+      for (const value of mars.getAll().values()) {
+        self.log(`${value.toString()} => ${beingAsEmoji(value.toString(true))}`);
+      }
+    }
+
+    const marsArr = [...mars.getAll().values()];
+    if (args.options.l) {
+      self.log(getMartians(marsArr, false, 'isAlive'));
+    }
+    if (args.options.r) {
+      self.log(getMartians(marsArr, 'Robot'));
+    }
+    if (args.options.m) {
+      self.log(getMartians(marsArr, 'Martian'));
+    }
+
+    callback();
+  });
 
 vorpal
   .command('instruct <x> <y> <orientation> <instructions> [name]')
@@ -46,6 +83,7 @@ vorpal
 vorpal
   .command('test')
   .description('Testing 1 3')
+  .hidden()
   .action(function(args, callback) {
     bounds.x = X_BOUNDS;
     bounds.y = Y_BOUNDS;
@@ -68,5 +106,30 @@ vorpal
   });
 
 vorpal
-  .delimiter('martians$')
+  .command('demo')
+  .description('Demo readme instructions.')
+  .action(function(args, callback) {
+    const self = this;
+
+    bounds.x = X_BOUNDS;
+    bounds.y = Y_BOUNDS;
+    const a = new Robot('a', 1, 1, 'E');
+    const b = new Robot('b', 3, 2, 'N');
+    const c = new Robot('c', 0, 3, 'W');
+    const aM = new Martian('aM', 3, 2, 'N');
+
+    mars.add(instruct(a, 'RFRFRFRF'));
+    mars.add(instruct(b, 'FRRFLLFFRRFLL'));
+    mars.add(instruct(c, 'LLFFFLFLFL'));
+    mars.add(instruct(aM, 'FRRFLLFFRRFLLFFF'));
+
+    for (const value of mars.getAll().values()) {
+      self.log(`${value.toString()} => ${beingAsEmoji(value.toString(true))}`);
+    }
+
+    callback();
+  });
+
+vorpal
+  .delimiter('martian-robots$')
   .show();
